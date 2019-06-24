12a4002b29e2bfe5017fe9088cb309f43edadb5e
Janis Pritzkau <janispritzkau@gmail.com>
2019-06-24 18:24:57 +0000

    Update examples/encode.ts

--- examples/encode.ts
+++ examples/encode.ts
@@ -6,9 +6,9 @@ const buffer = encode("root tag name", {
     float: new Float(0.75),
     double: 0.1 + 0.2,
     text: "Hello world",
-    longList: ["item 1", "item 2"],
+    list: ["item 1", "item 2"],
     nested: {
-        byteArray: Buffer.from([0x80, 0x40, 0x20])
+        byteArray: Buffer.from([0x80, 0x40, 0x20, 0x10])
     },
     intArray: new Int32Array([1, 2, 3, 4]),
     longArray: new BigInt64Array([1n, 2n, 3n, 4n]),
@@ -17,6 +17,3 @@ const buffer = encode("root tag name", {
 const { name, value } = decode(buffer)
 console.log(value)
 console.log(encode(name, value!).equals(buffer))
-
-console.log(decode(encode("", -1n)))
-// console.log(decode(encode("", 18446744073709551615n)))
