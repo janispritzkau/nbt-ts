b738e067a48989b55b21d8bb8c55b2c746a79984
Janis Pritzkau <janispritzkau@gmail.com>
2019-08-16 12:52:31 +0200

    Add tests

---
+++ test/index.js
@@ -0,0 +1,12 @@
+const assert = require("assert")
+const nbt = require("..")
+
+let tag = [{ string: "abc" }, { short: new nbt.Short(5) }, { big: 9990469871923500n }]
+let buffer = nbt.encode(null, tag)
+
+assert.deepStrictEqual(nbt.decode(buffer, false).value, tag)
+
+assert.throws(() => nbt.encode(null, [1, "a"]))
+assert.throws(() => nbt.decode(Buffer.from("00", "hex")))
+assert.throws(() => nbt.decode(Buffer.from("99", "hex"), false))
+assert.throws(() => nbt.encode(new Map))
